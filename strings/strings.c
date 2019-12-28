4698dd696d4c75c6f9cba859025032b21236714e
ddhorstman <davidzilla27@gmail.com>
2019-12-28 03:41:22 -0500

    Strings test seems broken, but code works.

--- strings/strings.c
+++ strings/strings.c
@@ -43,12 +43,15 @@ int main(void)
 {
     char quote1[] = "Don't forget to be awesome";
     char quote2[] = "a man a plan a canal panama";
+    char quote3[] = "hello world";
 
     char rv[512];
 
     printf("The string 'Don't forget to be awesome' has %d characters.\n", string_length(quote1));
     printf("The string 'a man a plan a canal panama' reversed is: '%s'\n", reverse_string(rv, quote2));
     printf("The length of the reversed string is: %d\n", string_length(reverse_string(rv,quote2)));
+    printf("The length of the reversed string is: %d\n", strlen(reverse_string(rv,quote3)));
+
     return 0;
 }
 #endif
