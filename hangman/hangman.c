c4ca479a9bb2d68a4789ce372912185a8f98dfdf
David Horstman <davidzilla27@gmail.com>
2020-01-20 14:42:18 -0800

    Hangman: read word as argument.

--- hangman/hangman.c
+++ hangman/hangman.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+int strikes = 0;
+char *word;
 char HANG_STATES[7][10 * 9] =
 	{
 		"             +         +----     +----     +----     +----     +----     +----     +----  ",
@@ -11,16 +12,29 @@ char HANG_STATES[7][10 * 9] =
 		"             |         |         |         |         |         |         |  /      |  / \\ ",
 		"             |         |         |         |         |         |         |         |      ",
 		"/*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   /*****\\   "};
-void display(int strikes){
-for (int j = 0; j < 7; j++)
+void display_hang(int strikes)
+{
+	int end_idx = (strikes+1)*10;
+	int i;
+	for (int j = 0; j < 7; j++)
 	{
-		for (int i = strikes*10; i < (strikes+1)*10; i++)
+		for (i = strikes * 10; i < end_idx; i++)
 			printf("%c", HANG_STATES[j][i]);
 		printf("\n");
 	}
 }
 int main(int argc, char *argv[])
 {
-	display(3);
+	word = (char *) malloc(sizeof(*argv[1]));
+	strcpy(word,argv[1]);
+	while (1)
+	{
+		printf("%s\n",word);
+		scanf("%d", &strikes);
+		display_hang(strikes);
+		if (strikes == 8)
+			break;
+	}
+	free(word);
 	return 0;
 }
